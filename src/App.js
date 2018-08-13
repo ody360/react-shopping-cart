b3c4a2d58d4203c019c8e5645810ca5f1bb67811
Hedim Ramirez <ody360@gmail.com>
2018-08-13 15:12:32 -0700

    passed copyright property to footer component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <CartHeader />
          <CartItems />
-        <CartFooter />
+        <CartFooter copyright='2016'/>
       </div>
     );
   }
